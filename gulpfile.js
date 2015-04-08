ac0716207258f4a0e67640c2b2c4aa24cbbf66ca
Honza Bittner <the@honzabittner.cz>
2015-04-08 22:29:03 +0200

    Renamed scss/ folder to sass/. Fixed gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename');
+
+// Files to read.
+var paths = {
+    sass: 'src/sass/**/*.scss'
+};
+
+// Folders for export.
+var dirs = {
+    css: 'src/css/'
+};
+
+// Compile Sass, autoprefix properties, generate CSS.
+gulp.task('sass', function () {
+    return gulp.src(paths.sass)
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest(dirs.css));
+});
+
+// Watch ['Sass'] change in files and run it at begin.
+gulp.task('watch', ['sass'], function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// On default call watch.
+gulp.task('default', ['sass']);
