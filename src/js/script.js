c84f2481d74097fb6adf32ace34c6817644adae8
Honza Bittner <the@honzabittner.cz>
2015-04-10 08:47:02 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,3 @@
-var elements = ['section', 'main', 'article', 'aside', 'header', 'footer', 'nav', 'figure', 'figcaption'];
-for( var i = 0; i < elements.length; i++ ) { document.createElement(elements[i]); }
-
 $(function(){
     $('nav a').on('click', function(e){
         var odkaz    = $(this).attr('href'),
