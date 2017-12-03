1d2464c4cc9c3fc9b8189f19b16160072788509e
Honza Bittner <git@honzabittner.cz>
2017-12-03 11:52:58 +0100

    Simplify, use Firebase

---
+++ lib/src/record_entity.dart
@@ -0,0 +1,80 @@
+import 'package:intl/intl.dart';
+
+class RecordEntity {
+  final String title;
+  final String titleLink;
+  final String subtitle;
+  final String subtitleLink;
+  final String description;
+  final DateTime date;
+  final String language;
+
+  final DateFormat _formatter = new DateFormat('d. M. yyyy');
+
+  RecordEntity({
+    this.title,
+    this.titleLink,
+    this.subtitle,
+    this.subtitleLink,
+    this.description,
+    this.date,
+    this.language = 'cz',
+  }) {
+    if (title == null) throw new ArgumentError.notNull('title');
+    if (date == null) throw new ArgumentError.notNull('date');
+
+  }
+
+  RecordEntity.fromMap(Map map)
+      : this(
+          title: map['title'],
+          titleLink: map['titleLink'],
+          subtitle: map['subtitle'],
+          subtitleLink: map['subtitleLink'],
+          description: map['description'],
+          date: DateTime.parse(map['date'] ?? ''),
+          language: map['language'],
+        );
+
+  @override
+  String toString() {
+    String titleElement = '', subtitleElement = '', descriptionElement = '';
+
+    bool planned = date.isAfter(new DateTime.now());
+
+    // link or simple text
+    if (titleLink != null) {
+      titleElement = '<a class="record__title link" href="${titleLink}" target="_blank">${title ?? ''}</a>';
+    } else {
+      titleElement = '<span class="record__title ${planned ? 'record__title--todo' : ''}">${title ?? ''}</span>';
+    }
+
+    // link or simple text
+    if (subtitle != null) {
+      subtitleElement = '<a class="record__event link" href="${subtitleLink}" target="_blank">${subtitle ?? ''}</a>';
+    } else {
+      subtitleElement = '<span class="record__event">${subtitle ?? ''}</span>';
+    }
+
+    // link or simple text
+    if (description != null) {
+      descriptionElement = '<p class="record__description">${description}</p>';
+    }
+
+    return '''
+      <div class="record">
+        <div class="record__header">
+          <div class="record__primary">
+            ${titleElement}
+            ${subtitleElement}
+          </div>
+          <div class="record__secondary">
+            <div class="record__date">${date != null ? _formatter.format(date) : ''}</div>
+            <span class="record__language">${language ?? ''}</span>
+          </div>
+        </div>
+        ${descriptionElement}
+      </div>
+    ''';
+  }
+}
--- web/main.dart
+++ web/main.dart
@@ -1,115 +1,45 @@
+import 'dart:async';
 import 'dart:html';
-import 'package:fckng_portfolio/article_entity.dart';
-import 'package:fckng_portfolio/project_entity.dart';
-import 'package:fckng_portfolio/talk_entity.dart';
+
+import 'package:fckng_portfolio/src/record_entity.dart';
+import 'package:firebase/firebase.dart' as fb;
 
 void main() {
-  initTalks();
-  initArticles();
-  initProjects();
+  fb.initializeApp(
+    apiKey: "AIzaSyBF5SuAklwamnRoSoVak-KmNUTEOe9CaWs",
+    authDomain: "portfolio-348b6.firebaseapp.com",
+    databaseURL: "https://portfolio-348b6.firebaseio.com",
+    storageBucket: "portfolio-348b6.appspot.com");
+
+  fb.Database database = fb.database();
+
+  initRecords(database.ref("talks"), '#talks');
+  initRecords(database.ref("articles"), '#articles');
+  initRecords(database.ref("projects"), '#projects');
 }
 
-void initTalks() {
-  DivElement element = querySelector("#talks");
+Future initRecords(fb.DatabaseReference ref, String selector) async {
+  DivElement element = querySelector(selector);
 
-  List<TalkEntity> talks = [
-    new TalkEntity(
-      name: "Budoucí přítomnost v CSS: grid a proměnné",
-      eventName: 'Přednáškový sraz ITnetwork',
-      eventLink: 'https://www.itnetwork.cz/nezarazene/prednaskovy-sraz-itnetwork/detail/153',
-      date: new DateTime(2018, 1, 26),
-    ),
-    new TalkEntity(
-      name: "Slyšeli jste někdy o Dartu?",
-      link: 'https://goo.gl/bTX3CB',
-      eventName: 'Přednáškový sraz ITnetwork',
-      eventLink: 'https://www.itnetwork.cz/nezarazene/prednaskovy-sraz-itnetwork/detail/110',
-      date: new DateTime(2017, 9, 29),
-    ),
-  ];
+  if (element == null) return;
 
-  talks.sort((TalkEntity a, TalkEntity b) => -a.date.compareTo(b.date));
+  List<RecordEntity> entities = [];
 
-  talks.forEach((TalkEntity entity) {
-    element.appendHtml(entity.toString(), treeSanitizer: NodeTreeSanitizer.trusted);
+  fb.QueryEvent qe = await ref.once('value');
+
+  (qe?.snapshot?.val() as Map)?.forEach((String key, Map value) {
+    try {
+      entities.add(new RecordEntity.fromMap(value));
+    } catch(e) {}
   });
-}
 
-void initArticles() {
-  DivElement element = querySelector("#articles");
-
-  List<ArticleEntity> articles = [
-    new ArticleEntity(
-      name: 'Základy jQuery',
-      link: 'https://www.itnetwork.cz/javascript/jquery-zaklady',
-      description: 'Series of articles on basics of jQuery.',
-      date: new DateTime(2014, 11, 11),
-    ),
-    new ArticleEntity(
-      name: 'Moderní a profesionální webové portfolio',
-      link: 'https://www.itnetwork.cz/html-css/webove-portfolio',
-      description: 'Series of articles on how to make modern portfolio.',
-      date: new DateTime(2014, 12, 21),
-    ),
-    new ArticleEntity(
-      name: 'Responzivní webdesign',
-      link: 'https://www.itnetwork.cz/html-css/responzivni-webdesign',
-      description: 'Series of articles on responsive design.',
-      date: new DateTime(2015, 4, 21),
-    ),
-    new ArticleEntity(
-      name: 'Technologie pro vývoj webových aplikací',
-      link: 'https://www.itnetwork.cz/html-css/webove-aplikace',
-      description: 'Series of articles as web export of my highschool semi-thesis about web technologies.',
-      date: new DateTime(2015, 5, 29),
-    ),
-    new ArticleEntity(
-      name: 'Hotová řešení v Javascriptu',
-      link: 'https://www.itnetwork.cz/javascript/jquery-hotova-reseni',
-      description: 'Series of articles on ready-made jQuery solutions.',
-      date: new DateTime(2016, 1, 25),
-    ),
-    new ArticleEntity(
-      name: 'Základní konstrukce jazyka Dart',
-      date: new DateTime(2017, 12, 24),
-    ),
-    new ArticleEntity(
-      name: 'Objektově orientované programování v Dart',
-      date: new DateTime(2017, 12, 25),
-    ),
-    new ArticleEntity(
-      name: 'AngularDart',
-      date: new DateTime(2018, 2, 1),
-    ),
-    new ArticleEntity(
-      name: 'Flutter',
-      date: new DateTime(2018, 4, 1),
-    ),
-  ];
-
-  articles.sort((ArticleEntity a, ArticleEntity b) => -a.date.compareTo(b.date));
-
-  if (articles.isEmpty) {
-    element?.appendHtml('<p class="paragraph">Nothing in the database yet.</p>');
-    return;
+  if (entities.isEmpty) {
+    element.appendHtml('<p class="paragraph">Nothing in the database yet.</p>');
   }
 
-  articles.forEach((ArticleEntity entity) {
-    element.appendHtml(entity.toString(), treeSanitizer: NodeTreeSanitizer.trusted);
-  });
-}
+  entities.sort((RecordEntity a, RecordEntity b) => -a.date.compareTo(b.date));
 
-void initProjects() {
-  DivElement element = querySelector("#projects");
-
-  List<ProjectEntity> projects = [];
-
-  if (projects.isEmpty) {
-    element?.appendHtml('<p class="paragraph">Nothing in the database yet.</p>');
-    return;
-  }
-
-  projects.forEach((ProjectEntity entity) {
+  entities.forEach((RecordEntity entity) {
     element.appendHtml(entity.toString(), treeSanitizer: NodeTreeSanitizer.trusted);
   });
 }
